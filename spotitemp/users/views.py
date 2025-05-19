dbfd39f9baff986a1b4311affaff5e432df5ad71
JENGADJY <JENGOERROR404@gmail.com>
2025-05-19 18:44:26 +0200

    cache

--- spotitemp/users/views.py
+++ spotitemp/users/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import redirect
